8589a54c33b8d2731d872eda4d8aceb8dd4639fc
Jianguo-Wang <wangjg36@mail.sysu.edu.cn>
2024-05-30 07:35:00 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='UBHDD',
+    version='1.0',
+    packages=['UBHDD'],
+    install_requires=[
+        'numpy',
+        'pandas',
+        'glmnet',
+        'scipy',
+    ],
+)
